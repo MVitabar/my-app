58a89f901ecd5c14a8ea25b5f32b995a9767f995
Martin <vitabarmartin@gmail.com>
2024-06-11 10:46:46 -0300

    app react native cep

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar,
+  SafeAreaView,
+  Image,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { styles } from "./style.js";
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  const [cep, setCep] = useState("");
+  const [result, setResult] = useState("");
+  async function askCep() {
+    const req = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+
+    const retorno = await req.json();
+    setResult("Endereço: " + retorno.logradouro);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>CONSULTA CEP</Text>
+      <Image
+        style={styles.img}
+        source={{
+          uri: "https://viacep.com.br/estatico/images/bt_doar_pix.png.pagespeed.ce.3xD2cQahH2.png",
+        }}
+      />
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o CEP"
+          style={styles.TextInput}
+          onChangeText={(text) => setCep(text)}
+        />
+        <TouchableOpacity style={styles.btn} onPress={askCep}>
+          <Text style={styles.btntxt}>CONSULTAR</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.result}>Resultado da busca: {result}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ style.js
@@ -0,0 +1,53 @@
+export const styles = {
+  container: {
+    backgroundColor: "#555",
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 50,
+  },
+
+  form: {
+    marginTop: 40,
+  },
+
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "#fff",
+  },
+
+  TextInput: {
+    backgroundColor: "#ececec",
+    padding: 15,
+    width: 200,
+    borderRadius: 6,
+    marginBottom: 20,
+    fontSize: 20,
+  },
+
+  result: {
+    fontSize: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    color: "#fff",
+  },
+
+  btn: {
+    backgroundColor: "#0000ff",
+    marginTop: 15,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+  },
+
+  btntxt: {
+    color: "#fff",
+    fontSize: 20,
+  },
+
+  img: {
+    width: 150,
+    height: 30,
+  },
+};
